faba5b8434ba88cb1ff4acb96f02b3dc15149fab
Chris Burge and Lauren Musante <chris@>
2019-09-16 11:25:47 -0700

    Add first three character cards.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 //Add import statements for classes and/or functions we write in frontend.js and backend.js
 
-$(document.ready(function) {
+$(document).ready(function() {
   //Call functions and instantiate classes written in frontend.js and backend.js
 });
