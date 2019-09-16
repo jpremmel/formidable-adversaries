3c3e4645c474ea3415dd3941893c87815f46135f
Jacqueline Remmel and Sofia Giordano <jacqueline@>
2019-09-16 11:38:38 -0700

    Add Match class with method to have computer pick a random attack.

--- src/backend.js
+++ src/backend.js
@@ -1,9 +1,36 @@
+export class Match {
+  constructor(userFighter, computerFighter) {
+    this.userFighter = userFighter;
+    this.computerFighter = computerFighter;
+  }
+  pickRandomAttack() { //method to randomly pick the computerFighter's attack
+    const number = Math.round(Math.random() * 2);
+    if (number === 0) {
+      this.userFighter.maxAttack();
+    } else if (number === 1) {
+      this.userFighter.medAttack();
+    } else if (number === 2) {
+      this.userFighter.minAttack();
+    }
+  }
+
+  rematch() {
+    this.userFighter.health = 50;
+    this.computerFighter.health = 50;
+  }
+}
+
 export class Fighter {
-  constructor (name) {
+  constructor(name) {
     this.name = name; //ex. "Bruce the Baboon"
     health = 50;
     atHome = false; //boolean, will be "true" when animal is on home turf
   }
+  setHealth() { //method to increase fighter's health slowly over time. make sure to call this method when the fight begins and clear it (using clearInterval()) when the fight ends.
+  setInterval(() => {
+    this.health++;
+  }, 1000);
+}
   maxAttack() { //methods for decreasing figher's health when receiving opponent's attacks
     this.health -= 10;
   }
@@ -12,10 +39,5 @@ export class Fighter {
   }
   minAttack() {
     this.health -= 5;
-  }
-  setHealth() { //method to increase fighter's health slowly over time. make sure to call this method when the fight begins and clear it (using clearInterval()) when the fight ends.
-    setInterval(() => {
-      this.health++;
-    }, 1000);
   }
 }
